4900841f4f6de865b947d69ccb3a37db2469d357
Kaua-Wallace <kauamelosilva68@gmail.com>
2025-09-28 00:02:30 -0300

    exercicio 4 115 finalizado

--- EDOO/exercicio 4 115/ex 4 115.cpp
+++ EDOO/exercicio 4 115/ex 4 115.cpp
@@ -4,39 +4,76 @@
 
 using namespace std;
 
-bool repetir_jogo = true;
+bool repeat_game = true;
 
 int main(){
-    int number, attpempt;
-    long tempo_sistema; // em segundos
-    srand((unsigned) tempo_sistema); // inicia o random numbers usando o tempo_sistema
+    int number, attempt;
+    long system_time; // em segundos
+
+    time(&system_time); // inicializa o system_time com o tempo atual
+    srand((unsigned) system_time); // inicia o random numbers usando o system_time
 
     cout << "\n\n "
         << " ******* A NUMERICAL GAME *******" << endl;
-cout << "\n\nRules of the game:" << endl;
+    cout << "\n\nRules of the game:" << endl;
 
-    while( repetir_jogo){
+    while (repeat_game){
         cout << "I have a number between 1 and 15 in mind \n"
             << "You have three chances to guess correctly!\n"
             << endl;
 
-        number = (rand() % 15) + 1; // rand() gera o númeroa ser acertado. rand() % 15 resulta em um valor de 0 a 14. Adicionar 1 garante que o intervalo seja de 1 a 15.
+        number = (rand() % 15) + 1; // rand() gera o número a ser acertado. rand() % 15 resulta em um valor de 0 a 14. Adicionar 1 garante que o intervalo seja de 1 a 15.
 
-        bool number_found = false;
-        int number_attempts = 0;
+        bool number_found = false; // número encontrado
+        int number_attempts = 0; // numero de tentativas
+        while (!number_found && number_attempts < 3){
 
+            // Limpa o buffer de entrada
+            cin.sync(); 
+            cin.clear();
+            cout << ++number_attempts << ". attempt: "; // aqui há o incremento do numero de tentativas
+            cin >> attempt;
+            if (attempt < number){
+                cout << "too small!" <<  endl;
+            }
 
+            else if (attempt > number){
+                cout << "too big!" << endl;
+            }
 
+            else { // note que resta apenas attempt==number, isto é, o numero foi encontrado
 
+                number_found = true;
+            }  
+        }
 
+        if (!number_found){
+            cout << "\nI won!"
+                << " The number in question was: "
+                << number << endl;
+        }
 
+        else {
+            cout << "\nCongratulations! You won!" << endl;
+        }
 
+        //pergunta se o jogador quer repetir
+
+        cout << "Want to repeat?\n";
+
+        char resposta_jogador;
+        cout << "Repeat ----> <r> Finish ----> <f>\n";
+
+        do {
+            cin.get(resposta_jogador); //O método get() da classe istream (associado ao objeto cin) é usado para ler o próximo caractere do stream de entrada (teclado) e armazená-lo na variável resposta_jogador        
+        } 
+        
+        while (resposta_jogador != 'r' && resposta_jogador != 'f');
+
+        if (resposta_jogador == 'f'){
+            repeat_game = false;
+        }    
     }
 
-
-
-
-
-
-
+    return 0;
 }
