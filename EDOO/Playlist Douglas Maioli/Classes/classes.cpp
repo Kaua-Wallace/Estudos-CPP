d9ff816039d11c1d1ce668b45e88be83377d599c
Kaua-Wallace <kauamelosilva68@gmail.com>
2025-10-07 20:36:57 -0300

    update_classes

--- EDOO/Playlist Douglas Maioli/Classes/classes.cpp
+++ EDOO/Playlist Douglas Maioli/Classes/classes.cpp
@@ -2,31 +2,20 @@
 
 using namespace std;
 
-class Propaganda{
-    private:
-
-
-    public:
-        void inscrever(){
-            cout << "Se inscreva no Canal do\nProfessor Douglas Maioli!\n";
-        }
-
-        void curtir(){
-            cout << "Curta o video!" << endl;
-        }
-
-};
-
-
-
-
-
 class Carro{
     private:
         int ano;
         float velocidade, preco;
          
     public:
+
+    //construtor
+        Carro(  int a = 0, float km = 0, float v = 0){
+            ano = a;
+            velocidade = km;
+            preco = v;
+        }
+
         void setano( int a){
             ano = a;
         }
@@ -54,12 +43,17 @@ class Carro{
 
 };
 
-int main() {
-    Propaganda canal;
-    canal.inscrever();
-    canal.curtir();
 
-    Carro palio;
+
+
+
+int main() {
+
+    // com o contrutor, teremos valores iniciais definidos para os atributos:
+    // Carro palio(2010, 144, 25500.50);
+    // nesse caso, não precisamos usar os sets para definir valores
+
+    Carro palio; // objeto da classe Carro: palio
     palio.setano(2010);
     cout << palio.getano() << endl;
 
@@ -68,6 +62,18 @@ int main() {
 
     palio.setvelocidade(144);
     cout << palio.getvelocidade() << endl;
+
+
+    // Carro celta(1999, 120, 31000.50);
+    Carro celta; // objeto da classe Carro: celta
+    celta.setano(1999);
+    cout << celta.getano() << endl;
+
+    celta.setpreco(31000.50);
+    cout << celta.getpreco() << endl;
+
+    celta.setvelocidade(120);
+    cout << celta.getvelocidade() << endl;
     
     return 0;
 }
